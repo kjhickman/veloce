73f1ea18c938b469f59af5160912be1c133edceb
Kyle Hickman <kyle@txhickman.com>
2025-03-06 23:53:44 -0600

    simplify transposition table

--- src/Zugfish.Engine/TranspositionTable.cs
+++ src/Zugfish.Engine/TranspositionTable.cs
@@ -12,7 +12,7 @@ public enum NodeType
 public struct TranspositionEntry
 {
     // A key of 0 is assumed to mean the slot is unused.
-    public ulong Key;
+    public ulong Hash;
     public int Depth;
     public int Score;
     public NodeType Flag;
@@ -39,63 +39,16 @@ public class TranspositionTable
         Array.Clear(_table, 0, _table.Length);
     }
 
-    public void Store(ulong key, int depth, int score, NodeType flag, Move bestMove)
+    public void Store(ulong hash, int depth, int score, NodeType flag, Move bestMove)
     {
-        var index = (int)(key & (ulong)_mask);
-        var bestReplaceIndex = -1;
-        var bestReplaceDepth = int.MaxValue;
+        var index = (int)(hash & (ulong)_mask);
+        var entry = _table[index];
 
-        // Only probe up to MaxProbes slots, consistent with TryGet
-        for (int i = 0; i < 4; i++)
+        if (_table[index].Hash == 0 || depth >= entry.Depth)
         {
-            // If the slot is empty, we can store the new entry here.
-            if (_table[index].Key == 0)
+            _table[index] = new TranspositionEntry
             {
-                _table[index] = new TranspositionEntry
-                {
-                    Key = key,
-                    Depth = depth,
-                    Score = score,
-                    Flag = flag,
-                    BestMove = bestMove
-                };
-                return;
-            }
-
-            // If the slot already contains the same key, update it if the new depth is better.
-            if (_table[index].Key == key)
-            {
-                if (depth >= _table[index].Depth)
-                {
-                    _table[index] = new TranspositionEntry
-                    {
-                        Key = key,
-                        Depth = depth,
-                        Score = score,
-                        Flag = flag,
-                        BestMove = bestMove
-                    };
-                }
-                return;
-            }
-
-            // Track the slot with the lowest depth as a replacement candidate
-            if (_table[index].Depth < bestReplaceDepth)
-            {
-                bestReplaceDepth = _table[index].Depth;
-                bestReplaceIndex = index;
-            }
-
-            index = (index + 1) & _mask;
-        }
-
-        // If no empty slot or matching key was found within MaxProbes,
-        // replace the entry with the lowest depth if the new depth is higher
-        if (bestReplaceIndex != -1 && depth > bestReplaceDepth)
-        {
-            _table[bestReplaceIndex] = new TranspositionEntry
-            {
-                Key = key,
+                Hash = hash,
                 Depth = depth,
                 Score = score,
                 Flag = flag,
@@ -104,49 +57,14 @@ public class TranspositionTable
         }
     }
 
-    public bool TryGet(ulong key, out TranspositionEntry entry)
+    public bool TryGet(ulong hash, out TranspositionEntry entry)
     {
-        var index = (int)(key & (ulong)_mask);
-        var i = 0;
-
-        while (i++ < 4)
+        var index = (int)(hash & (ulong)_mask);
+        if (_table[index].Hash == hash)
         {
-            if (_table[index].Key == key)
-            {
-                entry = _table[index];
-                return true;
-            }
-            if (_table[index].Key == 0)
-            {
-                entry = default;
-                return false;
-            }
-            index = (index + 1) & _mask;
+            entry = _table[index];
+            return true;
         }
-
-        entry = default;
-        return false;
-    }
-
-    public bool TryGet2(ulong key, out TranspositionEntry entry)
-    {
-        var index = (int)(key & (ulong)_mask);
-        var startIndex = index;
-
-        do
-        {
-            if (_table[index].Key == key)
-            {
-                entry = _table[index];
-                return true;
-            }
-            if (_table[index].Key == 0)
-            {
-                entry = default;
-                return false;
-            }
-            index = (index + 1) & _mask;
-        } while (index != startIndex);
 
         entry = default;
         return false;
