b9f87046712c9be46a8d64628aee99e3009714b3
Kyle Hickman <kyle@txhickman.com>
2026-06-04 22:05:31 -0500

    feat: add tapered evaluation

--- src/Veloce.UnitTests/MaterialEvaluatorTests.cs
+++ src/Veloce.UnitTests/MaterialEvaluatorTests.cs
@@ -14,4 +14,40 @@ public class MaterialEvaluatorTests
         await Assert.That(whiteToMove.Evaluate()).IsGreaterThan(0);
         await Assert.That(blackToMove.Evaluate()).IsGreaterThan(0);
     }
+
+    [Test]
+    public async Task Evaluate_InEndgame_PrefersCentralKing()
+    {
+        var centralKing = Fen.Parse("7k/8/8/8/4K3/8/8/8 w - - 0 1");
+        var cornerKing = Fen.Parse("7k/8/8/8/8/8/8/K7 w - - 0 1");
+
+        await Assert.That(centralKing.Evaluate()).IsGreaterThan(cornerKing.Evaluate());
+    }
+
+    [Test]
+    public async Task Evaluate_InMiddlegame_PrefersSaferKing()
+    {
+        var saferKing = Fen.Parse("rnbqkbnr/8/8/8/8/8/8/RNBQ1RK1 w - - 0 1");
+        var exposedKing = Fen.Parse("rnbqkbnr/8/8/8/4K3/8/8/RNBQ1R2 w - - 0 1");
+
+        await Assert.That(saferKing.Evaluate()).IsGreaterThan(exposedKing.Evaluate());
+    }
+
+    [Test]
+    public async Task Evaluate_InEndgame_PrefersAdvancedPawn()
+    {
+        var advancedPawn = Fen.Parse("7k/8/4P3/8/8/8/8/K7 w - - 0 1");
+        var startingPawn = Fen.Parse("7k/8/8/8/8/8/4P3/K7 w - - 0 1");
+
+        await Assert.That(advancedPawn.Evaluate()).IsGreaterThan(startingPawn.Evaluate());
+    }
+
+    [Test]
+    public async Task Evaluate_InEndgame_PrefersActiveRook()
+    {
+        var activeRook = Fen.Parse("7k/R7/8/8/8/8/8/6K1 w - - 0 1");
+        var passiveRook = Fen.Parse("7k/8/8/8/8/8/8/R5K1 w - - 0 1");
+
+        await Assert.That(activeRook.Evaluate()).IsGreaterThan(passiveRook.Evaluate());
+    }
 }
--- src/Veloce/Evaluation/MaterialEvaluator.cs
+++ src/Veloce/Evaluation/MaterialEvaluator.cs
@@ -10,20 +10,33 @@ public static class MaterialEvaluator
     internal const int BishopValue = 330;
     internal const int RookValue = 500;
     internal const int QueenValue = 900;
+    private const int MaxGamePhase = 24;
 
-    private static readonly int[] PawnTable =
+    private static readonly int[] MiddleGamePawnTable =
     [
          0,  0,  0,  0,  0,  0,  0,  0,
-        50, 50, 50, 50, 50, 50, 50, 50,
-        10, 10, 20, 30, 30, 20, 10, 10,
-         5,  5, 10, 25, 25, 10,  5,  5,
-         0,  0,  0, 20, 20,  0,  0,  0,
-         5, -5,-10,  0,  0,-10, -5,  5,
          5, 10, 10,-20,-20, 10, 10,  5,
+         5, -5,-10,  0,  0,-10, -5,  5,
+         0,  0,  0, 20, 20,  0,  0,  0,
+         5,  5, 10, 25, 25, 10,  5,  5,
+        10, 10, 20, 30, 30, 20, 10, 10,
+        50, 50, 50, 50, 50, 50, 50, 50,
          0,  0,  0,  0,  0,  0,  0,  0,
     ];
 
-    private static readonly int[] KnightTable =
+    private static readonly int[] EndGamePawnTable =
+    [
+          0,   0,   0,   0,   0,   0,   0,   0,
+          0,   0,   0, -10, -10,   0,   0,   0,
+          5,   5,   5,  10,  10,   5,   5,   5,
+         10,  10,  15,  25,  25,  15,  10,  10,
+         20,  20,  25,  35,  35,  25,  20,  20,
+         40,  40,  45,  50,  50,  45,  40,  40,
+         80,  80,  80,  80,  80,  80,  80,  80,
+          0,   0,   0,   0,   0,   0,   0,   0,
+    ];
+
+    private static readonly int[] MiddleGameKnightTable =
     [
         -50,-40,-30,-30,-30,-30,-40,-50,
         -40,-20,  0,  0,  0,  0,-20,-40,
@@ -35,52 +48,112 @@ public static class MaterialEvaluator
         -50,-40,-30,-30,-30,-30,-40,-50,
     ];
 
-    private static readonly int[] BishopTable =
+    private static readonly int[] EndGameKnightTable =
+    [
+        -50,-40,-30,-30,-30,-30,-40,-50,
+        -40,-20,  0,  5,  5,  0,-20,-40,
+        -30,  0, 10, 15, 15, 10,  0,-30,
+        -30,  5, 15, 20, 20, 15,  5,-30,
+        -30,  0, 15, 20, 20, 15,  0,-30,
+        -30,  5, 10, 15, 15, 10,  5,-30,
+        -40,-20,  0,  0,  0,  0,-20,-40,
+        -50,-40,-30,-30,-30,-30,-40,-50,
+    ];
+
+    private static readonly int[] MiddleGameBishopTable =
     [
         -20,-10,-10,-10,-10,-10,-10,-20,
-        -10,  0,  0,  0,  0,  0,  0,-10,
-        -10,  0,  5, 10, 10,  5,  0,-10,
-        -10,  5,  5, 10, 10,  5,  5,-10,
-        -10,  0, 10, 10, 10, 10,  0,-10,
-        -10, 10, 10, 10, 10, 10, 10,-10,
         -10,  5,  0,  0,  0,  0,  5,-10,
+        -10, 10, 10, 10, 10, 10, 10,-10,
+        -10,  0, 10, 10, 10, 10,  0,-10,
+        -10,  5,  5, 10, 10,  5,  5,-10,
+        -10,  0,  5, 10, 10,  5,  0,-10,
+        -10,  0,  0,  0,  0,  0,  0,-10,
         -20,-10,-10,-10,-10,-10,-10,-20,
     ];
 
-    private static readonly int[] RookTable =
+    private static readonly int[] EndGameBishopTable =
     [
-         0,  0,  0,  0,  0,  0,  0,  0,
-         5, 10, 10, 10, 10, 10, 10,  5,
-        -5,  0,  0,  0,  0,  0,  0, -5,
-        -5,  0,  0,  0,  0,  0,  0, -5,
-        -5,  0,  0,  0,  0,  0,  0, -5,
-        -5,  0,  0,  0,  0,  0,  0, -5,
-        -5,  0,  0,  0,  0,  0,  0, -5,
-         0,  0,  0,  5,  5,  0,  0,  0,
-    ];
-
-    private static readonly int[] QueenTable =
-    [
-        -20,-10,-10, -5, -5,-10,-10,-20,
+        -20,-10,-10,-10,-10,-10,-10,-20,
+        -10,  5,  0,  0,  0,  0,  5,-10,
+        -10, 10, 10, 10, 10, 10, 10,-10,
+        -10,  0, 10, 10, 10, 10,  0,-10,
+        -10,  5,  5, 10, 10,  5,  5,-10,
+        -10,  0,  5, 10, 10,  5,  0,-10,
         -10,  0,  0,  0,  0,  0,  0,-10,
-        -10,  0,  5,  5,  5,  5,  0,-10,
-         -5,  0,  5,  5,  5,  5,  0, -5,
-          0,  0,  5,  5,  5,  5,  0, -5,
-        -10,  5,  5,  5,  5,  5,  0,-10,
+        -20,-10,-10,-10,-10,-10,-10,-20,
+    ];
+
+    private static readonly int[] MiddleGameRookTable =
+    [
+         0,  0,  0,  5,  5,  0,  0,  0,
+         -5,  0,  0,  0,  0,  0,  0, -5,
+         -5,  0,  0,  0,  0,  0,  0, -5,
+         -5,  0,  0,  0,  0,  0,  0, -5,
+         -5,  0,  0,  0,  0,  0,  0, -5,
+         -5,  0,  0,  0,  0,  0,  0, -5,
+          5, 10, 10, 10, 10, 10, 10,  5,
+          0,  0,  0,  0,  0,  0,  0,  0,
+    ];
+
+    private static readonly int[] EndGameRookTable =
+    [
+        -10, -5,  0,  0,  0,  0, -5,-10,
+          0,  0,  5,  5,  5,  5,  0,  0,
+          0,  5, 10, 10, 10, 10,  5,  0,
+          0,  5, 10, 10, 10, 10,  5,  0,
+          0,  5, 10, 10, 10, 10,  5,  0,
+          0,  5, 10, 10, 10, 10,  5,  0,
+         10, 15, 15, 15, 15, 15, 15, 10,
+          0,  5,  5, 10, 10,  5,  5,  0,
+    ];
+
+    private static readonly int[] MiddleGameQueenTable =
+    [
+        -20,-10,-10, -5, -5,-10,-10,-20,
         -10,  0,  5,  0,  0,  0,  0,-10,
+        -10,  5,  5,  5,  5,  5,  0,-10,
+          0,  0,  5,  5,  5,  5,  0, -5,
+         -5,  0,  5,  5,  5,  5,  0, -5,
+        -10,  0,  5,  5,  5,  5,  0,-10,
+        -10,  0,  0,  0,  0,  0,  0,-10,
         -20,-10,-10, -5, -5,-10,-10,-20,
     ];
 
-    private static readonly int[] KingTable =
+    private static readonly int[] EndGameQueenTable =
     [
-        -30,-40,-40,-50,-50,-40,-40,-30,
-        -30,-40,-40,-50,-50,-40,-40,-30,
-        -30,-40,-40,-50,-50,-40,-40,-30,
-        -30,-40,-40,-50,-50,-40,-40,-30,
-        -20,-30,-30,-40,-40,-30,-30,-20,
-        -10,-20,-20,-20,-20,-20,-20,-10,
-         20, 20,  0,  0,  0,  0, 20, 20,
+        -20,-10,-10, -5, -5,-10,-10,-20,
+        -10,  0,  5,  0,  0,  0,  0,-10,
+        -10,  5,  5,  5,  5,  5,  0,-10,
+          0,  0,  5,  5,  5,  5,  0, -5,
+         -5,  0,  5,  5,  5,  5,  0, -5,
+        -10,  0,  5,  5,  5,  5,  0,-10,
+        -10,  0,  0,  0,  0,  0,  0,-10,
+        -20,-10,-10, -5, -5,-10,-10,-20,
+    ];
+
+    private static readonly int[] MiddleGameKingTable =
+    [
          20, 30, 10,  0,  0, 10, 30, 20,
+         20, 20,  0,  0,  0,  0, 20, 20,
+        -10,-20,-20,-20,-20,-20,-20,-10,
+        -20,-30,-30,-40,-40,-30,-30,-20,
+        -30,-40,-40,-50,-50,-40,-40,-30,
+        -30,-40,-40,-50,-50,-40,-40,-30,
+        -30,-40,-40,-50,-50,-40,-40,-30,
+        -30,-40,-40,-50,-50,-40,-40,-30,
+    ];
+
+    private static readonly int[] EndGameKingTable =
+    [
+        -50,-30,-20,-10,-10,-20,-30,-50,
+        -30,-10,  0, 10, 10,  0,-10,-30,
+        -20,  0, 20, 30, 30, 20,  0,-20,
+        -10, 10, 30, 40, 40, 30, 10,-10,
+        -10, 10, 30, 40, 40, 30, 10,-10,
+        -20,  0, 20, 30, 30, 20,  0,-20,
+        -30,-10,  0, 10, 10,  0,-10,-30,
+        -50,-30,-20,-10,-10,-20,-30,-50,
     ];
 
     public static int Evaluate(this Position position)
@@ -102,37 +175,57 @@ public static class MaterialEvaluator
 
     private static int EvaluatePosition(Position position)
     {
-        var positionalScore = 0;
-        positionalScore += EvaluatePiecePositions(position.WhitePawns, PawnTable, true);
-        positionalScore += EvaluatePiecePositions(position.WhiteKnights, KnightTable, true);
-        positionalScore += EvaluatePiecePositions(position.WhiteBishops, BishopTable, true);
-        positionalScore += EvaluatePiecePositions(position.WhiteRooks, RookTable, true);
-        positionalScore += EvaluatePiecePositions(position.WhiteQueens, QueenTable, true);
-        positionalScore += EvaluatePiecePositions(position.WhiteKing, KingTable, true);
+        var middleGameScore = 0;
+        var endGameScore = 0;
 
-        positionalScore -= EvaluatePiecePositions(position.BlackPawns, PawnTable, false);
-        positionalScore -= EvaluatePiecePositions(position.BlackKnights, KnightTable, false);
-        positionalScore -= EvaluatePiecePositions(position.BlackBishops, BishopTable, false);
-        positionalScore -= EvaluatePiecePositions(position.BlackRooks, RookTable, false);
-        positionalScore -= EvaluatePiecePositions(position.BlackQueens, QueenTable, false);
-        positionalScore -= EvaluatePiecePositions(position.BlackKing, KingTable, false);
+        AddPiecePositions(position.WhitePawns, MiddleGamePawnTable, EndGamePawnTable, true, ref middleGameScore, ref endGameScore);
+        AddPiecePositions(position.WhiteKnights, MiddleGameKnightTable, EndGameKnightTable, true, ref middleGameScore, ref endGameScore);
+        AddPiecePositions(position.WhiteBishops, MiddleGameBishopTable, EndGameBishopTable, true, ref middleGameScore, ref endGameScore);
+        AddPiecePositions(position.WhiteRooks, MiddleGameRookTable, EndGameRookTable, true, ref middleGameScore, ref endGameScore);
+        AddPiecePositions(position.WhiteQueens, MiddleGameQueenTable, EndGameQueenTable, true, ref middleGameScore, ref endGameScore);
+        AddPiecePositions(position.WhiteKing, MiddleGameKingTable, EndGameKingTable, true, ref middleGameScore, ref endGameScore);
 
-        return positionalScore;
+        AddPiecePositions(position.BlackPawns, MiddleGamePawnTable, EndGamePawnTable, false, ref middleGameScore, ref endGameScore);
+        AddPiecePositions(position.BlackKnights, MiddleGameKnightTable, EndGameKnightTable, false, ref middleGameScore, ref endGameScore);
+        AddPiecePositions(position.BlackBishops, MiddleGameBishopTable, EndGameBishopTable, false, ref middleGameScore, ref endGameScore);
+        AddPiecePositions(position.BlackRooks, MiddleGameRookTable, EndGameRookTable, false, ref middleGameScore, ref endGameScore);
+        AddPiecePositions(position.BlackQueens, MiddleGameQueenTable, EndGameQueenTable, false, ref middleGameScore, ref endGameScore);
+        AddPiecePositions(position.BlackKing, MiddleGameKingTable, EndGameKingTable, false, ref middleGameScore, ref endGameScore);
+
+        var phase = EvaluateGamePhase(position);
+        return (middleGameScore * phase + endGameScore * (MaxGamePhase - phase)) / MaxGamePhase;
     }
 
-    private static int EvaluatePiecePositions(Bitboard pieces, int[] table, bool isWhite)
+    private static void AddPiecePositions(
+        Bitboard pieces,
+        int[] middleGameTable,
+        int[] endGameTable,
+        bool isWhite,
+        ref int middleGameScore,
+        ref int endGameScore)
     {
-        var score = 0;
         var remainingPieces = pieces;
         while (remainingPieces.IsNotEmpty())
         {
             var square = remainingPieces.GetFirstSquare();
             var index = isWhite ? (int)square : 63 - (int)square;
-            score += table[index];
+            var sign = isWhite ? 1 : -1;
+
+            middleGameScore += sign * middleGameTable[index];
+            endGameScore += sign * endGameTable[index];
 
             remainingPieces &= remainingPieces - 1;
         }
+    }
 
-        return score;
+    private static int EvaluateGamePhase(Position position)
+    {
+        var phase = 0;
+        phase += position.WhiteKnights.Count() + position.BlackKnights.Count();
+        phase += position.WhiteBishops.Count() + position.BlackBishops.Count();
+        phase += 2 * (position.WhiteRooks.Count() + position.BlackRooks.Count());
+        phase += 4 * (position.WhiteQueens.Count() + position.BlackQueens.Count());
+
+        return Math.Min(phase, MaxGamePhase);
     }
 }
